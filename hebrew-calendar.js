23c34048616118e8bc900fa78e01ff61181a2bbe
Noah Lively <noah@koremedia.net>
2012-10-16 23:49:24 -0700

    Hebrew calendar now pulls from HebCal api

--- hebrew-calendar.js
+++ hebrew-calendar.js
@@ -1,89 +1,86 @@
-
 /* KaluachJS - Kaluach Javascript Hebrew/civil calendar
- *   Version 1.00
- * Copyright (C) 5760,5761 (2000 CE), by Abu Mami and Yisrael Hersch.
- *   All Rights Reserved.
- *   All copyright notices in this script must be left intact.
- * Requires kdate.js - Kaluach Javascript Hebrew date routines
- * Acknowledgment given to scripts by:
- *	 - Tomer and Yehuda Shiran (docjs.com)
- *   - Gordon McComb
- *   - irt.org
- *   - javascripter.net
- * Terms of use:
- *   - Permission will be granted to use this script on personal
- *     web pages. All that's required is that you please ask.
- *     (Of course if you want to send a few dollars, that's OK too :-)
- *   - Use on commercial web sites requires a $50 payment.
- * website: http://www.kaluach.net
- * email: abumami@kaluach.net
- */
+*   Version 1.00
+* Copyright (C) 5760,5761 (2000 CE), by Abu Mami and Yisrael Hersch.
+*   All Rights Reserved.
+*   All copyright notices in this script must be left intact.
+* Requires kdate.js - Kaluach Javascript Hebrew date routines
+* Acknowledgment given to scripts by:
+*	 - Tomer and Yehuda Shiran (docjs.com)
+*   - Gordon McComb
+*   - irt.org
+*   - javascripter.net
+* Terms of use:
+*   - Permission will be granted to use this script on personal
+*     web pages. All that's required is that you please ask.
+*     (Of course if you want to send a few dollars, that's OK too :-)
+*   - Use on commercial web sites requires a $50 payment.
+* website: http://www.kaluach.net
+* email: abumami@kaluach.net
+*/
+
+//pads left
+String.prototype.lpad = function(padString, length) {
+  var str = this;
+  while (str.length < length)
+    str = padString + str;
+  return str;
+}
+//pads right
+String.prototype.rpad = function(padString, length) {
+  var str = this;
+  while (str.length < length)
+    str = str + padString;
+  return str;
+}
 
 $(document).ready(function() {
 
-  var otherHolidays = 0;
-  var jewishHolidays = 1;
-  var civilHolidays = 0;
-  
+  var events = {};
+  var hasData = {};
+
+  /**
+	 * Declare selectors
+	 */
+
   var calendarContainer = $('#calendar-container');
   var calendarForm = $('#calendar-form');
-  
-  var holidaysSelect = $('#holidays', calendarForm);
+
   var yearField = $('#year', calendarForm);
   var monthField = $('#month', calendarForm);
-  var prevMonthButton = $('#prev-month', calendarForm);
-  var nextMonthButton = $('#next-month', calendarForm);
-  var prevYearButton = $('#prev-year', calendarForm);
-  var nextYearButton = $('#next-year', calendarForm);
-  var todayButton = $('#today', calendarForm);
 
-  var isMobile = function() {
-    return $('body').is('.mobile');
-  }; 
+  var hasDataForYear = function(year) {
+    return (hasData[year] != null);
+  }
+  /**
+	 * Event handlers
+	 */
 
-  var initialize = function() {
-    
-    // holidaysSelect.change(holidaysSelect_Change);
-    // todayButton.click(todayButton_Click);
-    // prevMonthButton.click(prevMonthButton_Click);
-    // nextMonthButton.click(nextMonthButton_Click);
-    // prevYearButton.click(prevYearButton_Click);
-    // nextYearButton.click(nextYearButton_Click);
-  
-    // yearField.change(yearField_Change);
-    // monthField.change(monthField_Change);
+  var data_Retrieved = function(data) {
+    if (!hasDataForYear(getSelectedYear())) {
+      for (var x in data.items) {
+        var item = data.items[x];
+        if (events[item.date] == undefined) {
+          events[item.date] = [];
+        }
 
-    selectToday();
+        events[item.date].push(item);
+      }
+      hasData[getSelectedYear()] = getSelectedYear();
+
+      console.log(hasData);
+    }
+    renderEvents();
   };
-  
-  var yearField_Change = function() {
-    setCalendarFromMonthYear();
+
+  var data_Error = function(e) {
+
   };
-  
-  var monthField_Change = function() {
-    setCalendarFromMonthYear();
-  };
-  
-  var prevYearButton_Click = function() {
-    var y = getSelectedYear();
-    var m = getSelectedMonthIndex();
-    y -= 1;
-    
-    setCalendar(m, y);
-  };
-  
-  var nextYearButton_Click = function() {
-    var y = getSelectedYear();
-    var m = getSelectedMonthIndex();
-    y += 1;
-    setCalendar(m, y);
-  };
-  
+
   var prevMonthButton_Click = function() {
     var y = getSelectedYear();
     var m = getSelectedMonthIndex();
 
-    if(m > 0)
+    if (m > 0)
       m -= 1;
     else {
       m = 11;
@@ -92,12 +89,12 @@ $(document).ready(function() {
 
     setCalendar(m, y);
   };
-  
+
   var nextMonthButton_Click = function() {
     var y = getSelectedYear();
     var m = getSelectedMonthIndex();
 
-    if(m < 11)
+    if (m < 11)
       m++;
     else {
       m = 0;
@@ -105,57 +102,26 @@ $(document).ready(function() {
     }
 
     setCalendar(m, y);
-  };
-  
-  var todayButton_Click = function () {
-    selectToday();
-  };
-  
-  var holidaysSelect_Change = function() {
-    var y = getSelectedYear();
-    var m = getSelectedMonthIndex();
-    var h = holidaysSelect.get(0).selectedIndex;
-    if(h == 0) {
-      jewishHolidays = 1;
-      civilHolidays = 0;
-    }
-    else if(h == 1) {
-      jewishHolidays = 0;
-      civilHolidays = 1;
-    }
-    else if(h == 2) {
-      jewishHolidays = 1;
-      civilHolidays = 1;
-    }
-    else if(h == 3) {
-      jewishHolidays = 0;
-      civilHolidays = 0;
-    }
 
-    doCal(m, y);
-  }
-  
+  };
+
   var dayCell_Click = function() {
-  	var rel = $(this).attr('rel');
-  	var relDetail = $('.event-wrapper[rel="' + rel + '"]');
-  	if (relDetail.length > 0) {
-  		relDetail.get(0).scrollIntoView();
-  	}
+    var rel = $(this).attr('rel');
+    var relDetail = $('.event-wrapper[rel="' + rel + '"]');
+    if (relDetail.length > 0) {
+      relDetail.get(0).scrollIntoView();
+    }
   };
 
-  function toggleOther(form) {
-    otherHolidays = (otherHolidays == 1) ? 0 : 1;
-    var y = getSelectedYear();
-    var m = getSelectedMonthIndex();
-    doCal(m, y);
-  }
-
+  /**
+	 * Utility methods
+	 */
 
   function doCal(month, year) {
     var ret = calendar(month, year);
     var calendarTable = BuildLuachHTML(ret);
     calendarContainer.empty().append(calendarTable);
-    
+
     $('td.day').click(dayCell_Click);
   }
 
@@ -163,7 +129,7 @@ $(document).ready(function() {
     var m = selM + 1;
     var y = selY;
     var d = civMonthLength(m, y);
-    var firstOfMonth = new Date (y, selM, 1);
+    var firstOfMonth = new Date(y, selM, 1);
     var startPos = firstOfMonth.getDay() + 1;
     var retVal = new Object();
     retVal[1] = startPos;
@@ -173,16 +139,14 @@ $(document).ready(function() {
     return (retVal);
   }
 
-  function BuildLuachHTML(parms)  {
-    var cellWidth  = 80;			// width of columns in table
-    var cellHeight = 55;			// height of day cells calendar
+  function BuildLuachHTML(parms) {
     var hebDate;
     var hebDay;
     var now = new Date();
     var tday = now.getDate();
     var tmonth = now.getMonth();
     var tyear = now.getYear();
-    if(tyear < 1000)
+    if (tyear < 1000)
       tyear += 1900;
     var cMonth = parms[3];
     var cYear = parms[4];
@@ -194,21 +158,21 @@ $(document).ready(function() {
 
     // get starting Heb month in civil month
     hebDate = civ2heb(1, cMonth, cYear);
-    hmS = hebDate.substring(hebDate.indexOf(' ')+1, hebDate.length);
+    hmS = hebDate.substring(hebDate.indexOf(' ') + 1, hebDate.length);
     hMonth = eval(hmS.substring(0, hmS.indexOf(' ')));
-    hYear = hmS.substring(hmS.indexOf(' ')+1, hmS.length);
-    var start = hebMonth[hMonth+1] + ' ' + hYear;
+    hYear = hmS.substring(hmS.indexOf(' ') + 1, hmS.length);
+    var start = hebMonth[hMonth + 1] + ' ' + hYear;
 
     // get ending Heb month in civil month
     hebDate = civ2heb(lastDate, cMonth, cYear);
-    hmE = hebDate.substring(hebDate.indexOf(' ')+1, hebDate.length);
+    hmE = hebDate.substring(hebDate.indexOf(' ') + 1, hebDate.length);
     hMonth = eval(hmE.substring(0, hmE.indexOf(' ')));
-    hYear = hmE.substring(hmE.indexOf(' ')+1, hmE.length);
-    var end = hebMonth[hMonth+1] + ' ' + hYear;
+    hYear = hmE.substring(hmE.indexOf(' ') + 1, hmE.length);
+    var end = hebMonth[hMonth + 1] + ' ' + hYear;
 
     var hebSpan;
     // check if start and end Heb months are the same
-    if(hmS == hmE)
+    if (hmS == hmE)
       hebSpan = start;
     else
       hebSpan = start + ' / ' + end
@@ -219,22 +183,22 @@ $(document).ready(function() {
     var table2 = $('<table class="calendar table-body"></table>');
     var eventsList = $('<div id="calendar-list"></div>');
     var eventsListContent = '';
-    
+
     var tHead = $('<thead></thead>');
     var tBody = $('<tbody></tbody>');
-    
+
     var headerRow = $('<tr class="month-header"></tr>');
     var headerCell = $('<th colspan="7"></th>');
-    
+
     var headerText = '<span class="english">' + monthName + ' ' + cYear + '</span>';
     headerText += '<span class="hebrew">' + hebSpan + '</span>';
-    
+
     var prevMonthLink = $('<a id="previous-month"></a>').html('&lt;');
     var nextMonthLink = $('<a id="next-month"></a>').html('&gt;');
-    
+
     prevMonthLink.click(prevMonthButton_Click);
     nextMonthLink.click(nextMonthButton_Click);
-        
+
     headerCell.html(headerText).append(prevMonthLink).append(nextMonthLink);
     headerRow.append(headerCell);
     tHead.append(headerRow);
@@ -247,80 +211,65 @@ $(document).ready(function() {
       dayCell.text(weekDay[dayNum]);
       daysRow.append(dayCell);
     }
-    
+
     tHead.append(daysRow);
-    
+
     var cell = 1
     var cDay = 1
     var row;
-    for (row = 1; row <= 6; row++) {
-      
+    for ( row = 1; row <= 6; row++) {
+
       var weekRow = $('<tr class="week-row row-' + row + '" rel="' + cDay + '"></tr>');
-      
-      for (var col = 1; col <= 7; col++)  {
-        
+
+      for (var col = 1; col <= 7; col++) {
+
         var weekCell = $('<td class="day"></td>').attr('rel', cDay);
-        
 
         // convert civil date to hebrew
         hebDate = civ2heb(cDay, cMonth, cYear);
         hebDay = eval(hebDate.substring(0, hebDate.indexOf(' ')));
 
-        var hm = hebDate.substring(hebDate.indexOf(' ')+1, hebDate.length);
-        var hMonth = eval(hm.substring(0, hm.indexOf(' ')));
+        var dateString = cYear + '-' + cMonth.toString().lpad('0', 2) + '-' + cDay.toString().lpad('0', 2);
+        var eventsOnDate = events[dateString];
 
-        if (cell < parms[1]) {
-          
-        }
-        else {
-          
-          
-          var moed = "";
-          if(jewishHolidays)
-            moed = moadim(cDay, cMonth, cYear, hebDay, hMonth, col);
-          var holiday = "";
-          if(civilHolidays)
-            holiday = holidays(cDay, cMonth, cYear);
-          
-          var cellClass = "";
-          if((cDay == tday) && (parms[3] == (tmonth+1)) && (parms[4] == tyear))
-            cellClass = "current-day";
-          else if (moed != "")
-            cellClass = "holiday";
-          else if (holiday != "") {
-            cellClass = "civil-holiday";
-          }
-          
-          weekCell.addClass(cellClass);
+        hm = hebDate.substring(hebDate.indexOf(' ') + 1, hebDate.length);
+        hMonth = eval(hm.substring(0, hm.indexOf(' ')));
 
+        var cellClasses = ['day'];
+        if ((cDay == tday) && (parms[3] == (tmonth + 1)) && (parms[4] == tyear))
+          cellClasses.push("current-day");
 
+        var cellContents = '';
+        // assemble the contents of our day cell
+        cellContents += '<div class="cell-contents">';
+        cellContents += '<div class="english">';
+        cellContents += cDay;
+        cellContents += '</div>';
+        cellContents += '<div class="hebrew">';
+        cellContents += hebDay;
+        cellContents += '</div>';
 
-          var cellContents = '';
-          // assemble the contents of our day cell
-          cellContents += '<div class="cell-contents">';
-          cellContents +=       '<div class="english">';
-          cellContents +=           cDay;
-          cellContents +=       '</div>';
-          cellContents +=       '<div class="hebrew">';
-          cellContents +=           hebDay;
-          cellContents +=       '</div>';
-          
+        if (eventsOnDate != null && eventsOnDate.length > 0) {
+          cellClasses.push('with-data');
+
           var eventDetail = '';
-          if (moed != "")
-            eventDetail += moed;
-          if (moed != "" && holiday != "")
-            eventDetail += '<br>';
-          if (holiday != "")
-            eventDetail += holiday;
-            
+          var titles = [];
+          for (var d in eventsOnDate) {
+            titles.push(eventsOnDate[d].title + ' (' + eventsOnDate[d].hebrew + ')');
+            cellClasses.push(eventsOnDate[d].category);
+          }
+
+          cellClasses = cellClasses.join(' ');
+
+          eventDetail = titles.join('<br />');
+
           if (!isMobile()) {
             cellContents += '<div class="events">';
             cellContents += eventDetail;
             cellContents += '</div>';
-          }
-          else {
+          } else {
             if (eventDetail.length > 0) {
-              eventsListContent += '<div class="event-wrapper ' + cellClass + '" rel="' + cDay + '">';
+              eventsListContent += '<div class="event-wrapper ' + cellClasses + '" rel="' + cDay + '">';
               eventsListContent += '<div class="date">';
               eventsListContent += civMonth[cMonth] + ' ' + cDay;
               eventsListContent += ' / ';
@@ -332,14 +281,16 @@ $(document).ready(function() {
               eventsListContent += '</div>';
             }
           }
-          
-          cellContents += '</div>';
-          
-          weekCell.html(cellContents);
 
-          cDay++;
+          cellContents += '</div>';
+
         }
-        
+
+        weekCell.attr('class', cellClasses);
+        weekCell.html(cellContents);
+
+        cDay++;
+
         weekRow.append(weekCell);
 
         if (cDay <= lastDate)
@@ -350,54 +301,79 @@ $(document).ready(function() {
 
       tBody.append(weekRow);
 
-      if(cDay > parms[2])
+      if (cDay > parms[2])
         break;
     }
-    
+
     table1.append(tHead);
     table2.append(tBody);
-    
+
     eventsList.html(eventsListContent);
-    
+
     container.append(table1);
     container.append(table2);
     container.append(eventsList);
-    
-    
+
     return container;
   }
-  
+
   var getSelectedYear = function() {
     return parseInt(yearField.val());
   };
-  
+
   var getSelectedMonthIndex = function() {
     return monthField.get(0).selectedIndex;
   };
-  
-  
-  var setCalendarFromMonthYear = function () {
+
+  var setCalendarFromMonthYear = function() {
     var y = getSelectedYear();
     var m = getSelectedMonthIndex();
     setCalendar(m, y);
   }
-  
-  var setCalendar = function(month, year) {
+  var setCalendar = function(month, year, silent) {
     monthField.get(0).selectedIndex = month;
     yearField.val(year);
-    doCal(month, year);
+
+    if (!hasDataForYear(year)) {
+      fetchData();
+    }
+
+    if (!silent)
+      doCal(month, year);
   };
-  
-  var selectToday = function () {
+
+  var isMobile = function() {
+    return $('body').is('.mobile');
+  };
+
+  var initialize = function() {
     var now = new Date();
     var y = now.getYear();
     var m = now.getMonth();
-    if(y < 1000)
+    if (y < 1000)
       y += 1900;
 
-    setCalendar(m, y);
+    setCalendar(m, y, true);
+
+    fetchData();
   };
-  
+
+  var renderEvents = function() {
+    setCalendarFromMonthYear();
+  };
+
+  var fetchData = function() {
+    var url = 'http://www.hebcal.com/hebcal/?v=1;cfg=json;nh=on;nx=on;year=' + getSelectedYear() + ';month=x;ss=on;mf=on;c=on;m=72;s=on';
+
+    $.ajax({
+      url : url,
+      dataType : 'jsonp',
+      success : data_Retrieved,
+      error : data_Error
+    });
+
+  };
+
   initialize();
 
 });
